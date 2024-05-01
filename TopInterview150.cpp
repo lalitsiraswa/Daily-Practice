5f8f111959bbb49cb94ad16df50d7ec556d625f8
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-05-01 13:41:57 +0530

    Merge Sorted Array

--- TopInterview150.cpp
+++ TopInterview150.cpp
@@ -1,10 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// ------------------------------------------------------------------- 88. Merge Sorted Array ------------------------------------------------------------------
+void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+{
+    int index = (m + n) - 1;
+    int mPtr = --m, nPtr = --n;
+    while (nPtr >= 0)
+    {
+        if (mPtr < 0)
+        {
+            nums1[index] = nums2[nPtr--];
+        }
+        else if (nums2[nPtr] > nums1[mPtr])
+        {
+            nums1[index] = nums2[nPtr--];
+        }
+        else
+        {
+            nums1[index] = nums1[mPtr--];
+        }
+        index--;
+    }
+}
+void merge_2(vector<int> &nums1, int m, vector<int> &nums2, int n)
+{
+    int p1 = m - 1, p2 = n - 1;
+    int p = (m + n) - 1;
+    while (p1 > -1 && p2 > -1)
+    {
+        if (nums1[p1] > nums2[p2])
+            nums1[p--] = nums1[p1--];
+        else
+            nums1[p--] = nums2[p2--];
+    }
+    while (p2 > -1)
+    {
+        nums1[p--] = nums2[p2--];
+    }
+}
 int main()
 {
     cout << string(35, '-') << endl;
-    cout << "Hello World !" << endl;
+    vector<int> nums1 = {2, 0};
+    vector<int> nums2 = {1};
+    int m = 1, n = 1;
+    merge(nums1, m, nums2, n);
+    for (auto item : nums1)
+        cout << item << " ";
+    cout << endl;
     cout << string(35, '-');
     return 0;
 }
