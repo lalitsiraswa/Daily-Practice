d3d8a38cc6d87c86d4fb4eafae77dc07eda391df
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-05-12 18:20:22 +0530

    Java 8 Features

---
+++ JavaPractice/Java8Features.java
@@ -0,0 +1,13 @@
+// JAVA 8 Features
+// 1. Lambda Expressions
+// 2. Functional Interfaces
+// 3. Method Reference
+// 4. Streams
+// 5. Comparable and Comparator
+// 6. Optional Class
+// 7. Date/Time API
+public class Java8Features {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
