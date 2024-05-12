293284df8dbf8fec00f498c2fc04e30b038c1aa2
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-05-12 19:43:18 +0530

    Functional Interfaces in Java

--- JavaPractice/Java8Features.java
+++ JavaPractice/Java8Features.java
@@ -6,8 +6,53 @@
 // 5. Comparable and Comparator
 // 6. Optional Class
 // 7. Date/Time API
+
+// Java program to demonstrate functional interface
+@FunctionalInterface
+interface Square {
+    abstract int calculate(int x);
+}
+
 public class Java8Features {
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+        // create anonymous inner class object
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Before Java 8 'New thread created'");
+            }
+        }).start();
+
+        new Thread(() -> {
+            System.out.println("Java 8 onwards 'New thread created'");
+        }).start();
+
+        // Create an anonymous class object that extends the Thread class.
+        new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Hello from the anonymous class thread!");
+            }
+        }.start();
+
+        // Create an anonymous class object that implements the Runnable interface.
+        new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello from the anonymous class runnable!");
+            }
+        }.run();
+        // ------------------
+        int a = 5;
+        Square s = new Square() {
+            @Override
+            public int calculate(int a) {
+                return a * a;
+            }
+        };
+        System.out.println(s.calculate(a));
+        // -------------------
+        Square s1 = (int x) -> x * x;
+        System.out.println(s1.calculate(4));
     }
 }
