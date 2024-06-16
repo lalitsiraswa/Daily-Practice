1410ef7a07dc6492f84cf1b55ed40c1fb610ddf9
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-06-16 13:51:00 +0530

    15. 3Sum

--- TopInterview150.cpp
+++ TopInterview150.cpp
@@ -1887,7 +1887,7 @@ vector<int> twoSum2(vector<int> &numbers, int target)
     return {};
 }
 // ------------------------
-vector<int> twoSum(vector<int> &numbers, int target)
+vector<int> twoSum3(vector<int> &numbers, int target)
 {
     int left = 0, right = numbers.size() - 1;
     while (numbers[left] + numbers[right] != target)
@@ -1926,7 +1926,7 @@ int maxArea(vector<int> &height)
     return maxAmountWater;
 }
 // --------------------
-int maxArea(vector<int> &height)
+int maxArea2(vector<int> &height)
 {
     int n = height.size();
     int mostWaterLevel = 0;
@@ -1944,7 +1944,7 @@ int maxArea(vector<int> &height)
     return mostWaterLevel;
 }
 // --------------------
-int maxArea(vector<int> &height)
+int maxArea3(vector<int> &height)
 {
     int n = height.size();
     int mostWaterLevel = 0;
@@ -1960,6 +1960,165 @@ int maxArea(vector<int> &height)
             right--;
     }
     return mostWaterLevel;
+}
+// int main()
+// {
+//     cout << string(35, '-') << endl;
+//     cout << string(35, '-');
+//     return 0;
+// }
+// ------------------------------------------------------------------------ 15. 3Sum ----------------------------------------------------------------------
+// --------- TUF Solution ---------
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end()); // Sorted Array
+    if (nums.size() < 3)
+    { // Base case 1
+        return {};
+    }
+    if (nums[0] > 0)
+    { // Base case 2
+        return {};
+    }
+    vector<vector<int>> answer;
+    for (int i = 0; i < nums.size(); ++i)
+    { // Traversing the array to fix the number.
+        if (nums[i] > 0)
+        { // If number fixed is +ve, stop there because we can't make it zero by searching after it.
+            break;
+        }
+        if (i > 0 && nums[i] == nums[i - 1])
+        { // If number is getting repeated, ignore the lower loop and continue.
+            continue;
+        }
+        int low = i + 1, high = nums.size() - 1; // Make two pointers high and low, and initialize sum as 0.
+        int sum = 0;
+        while (low < high)
+        { // Search between two pointers, just similiar to binary search.
+            sum = nums[i] + nums[low] + nums[high];
+            if (sum > 0)
+            { // If sum is +ve, means, we need more -ve numbers to make it 0, decreament high (high--).
+                high--;
+            }
+            else if (sum < 0)
+            { // If sum is -ve, means, we need more +ve numbers to make it 0, increament low (low++).
+                low++;
+            }
+            else
+            {
+                answer.push_back({nums[i], nums[low], nums[high]});                   // we have found the required triplet, push it in answer vector
+                int last_low_occurence = nums[low], last_high_occurence = nums[high]; // Now again, to avoid duplicate triplets, we have to navigate to last occurences of num[low] and num[high] respectively
+                while (low < high && nums[low] == last_low_occurence)
+                { // Update the low and high with last occurences of low and high.
+                    low++;
+                }
+                while (low < high && nums[high] == last_high_occurence)
+                {
+                    high--;
+                }
+            }
+        }
+    }
+    return answer; // Return the answer vector.
+}
+// ---------------------
+vector<vector<int>> threeSum2(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end()); // Sorted Array
+    if (nums.size() < 3)
+    { // Base Case 1
+        return {};
+    }
+    if (nums[0] > 0)
+    { // Base Case 2
+        return {};
+    }
+    unordered_map<int, int> hashMap;
+    for (int i = 0; i < nums.size(); ++i)
+    { // Hashing of Indices
+        hashMap[nums[i]] = i;
+    }
+    vector<vector<int>> answer;
+    for (int i = 0; i < nums.size() - 2; ++i)
+    { // Traversing the array to fix the number.
+        if (nums[i] > 0)
+        { // If number fixed is +ve, stop there because we can't make it zero by searching after it.
+            break;
+        }
+        for (int j = i + 1; j < nums.size() - 1; ++j)
+        {                                            // Fixing another number after first number
+            int required = -1 * (nums[i] + nums[j]); // To make sum 0, we would require the -ve sum of both fixed numbers.
+            if (hashMap.count(required) && hashMap.find(required)->second > j)
+            { // If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet.
+                answer.push_back({nums[i], nums[j], required});
+            }
+            j = hashMap.find(nums[j])->second; // Update j to last occurence of 2nd fixed number to avoid duplicate triplets.
+        }
+        i = hashMap.find(nums[i])->second; // Update i to last occurence of 1st fixed number to avoid duplicate triplets.
+    }
+    return answer; // Return answer vector.
+}
+// ------- TLE --------
+vector<vector<int>> threeSum_TUF(vector<int> &nums)
+{
+    int n = nums.size();
+    set<vector<int>> st;
+    for (int i = 0; i < n; i++)
+    {
+        set<int> hashset;
+        for (int j = i + 1; j < n; j++)
+        {
+            // Calculate the 3rd element:
+            int third = -(nums[i] + nums[j]);
+            // Find the element in the set:
+            if (hashset.find(third) != hashset.end())
+            {
+                vector<int> temp = {nums[i], nums[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            }
+            hashset.insert(nums[j]);
+        }
+    }
+    // store the set in the answer:
+    vector<vector<int>> result(st.begin(), st.end());
+    return result;
+}
+// ----------------
+vector<vector<int>> threeSum_TUF_OtherWay(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<vector<int>> result;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < n; i++)
+    {
+        // remove duplicates:
+        if (i != 0 && nums[i] == nums[i - 1])
+            continue;
+        // moving 2 pointers:
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k)
+        {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum < 0)
+                j++;
+            else if (sum > 0)
+                k--;
+            else
+            {
+                result.push_back({nums[i], nums[j], nums[k]});
+                j++;
+                k--;
+                // skip the duplicates:
+                while (j < k && nums[j] == nums[j - 1])
+                    j++;
+                while (j < k && nums[k] == nums[k + 1])
+                    k--;
+            }
+        }
+    }
+    return result;
 }
 int main()
 {
