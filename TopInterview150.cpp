51ac99672bd9f217ad7d36095cf827a144a3fde3
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-07-07 12:19:28 +0530

    70. Climbing Stairs Revision

--- TopInterview150.cpp
+++ TopInterview150.cpp
@@ -4026,11 +4026,50 @@ int firstUniqChar2(string s)
             return i;
     return -1;
 }
+// int main()
+// {
+//     cout << string(35, '-') << endl;
+//     string s = "aabb";
+//     cout << firstUniqChar(s) << endl;
+//     cout << string(35, '-');
+//     return 0;
+// }
+// ------------------------------------------------------------------- 70. Climbing Stairs Revision -------------------------------------------------------------------
+int helperClimbStairs(int n, vector<int> &dp)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    if (dp[n] != -1)
+        return dp[n];
+    int smallAnswer = helperClimbStairs(n - 1, dp);
+    if (n >= 2)
+        smallAnswer += helperClimbStairs(n - 2, dp);
+    return dp[n] = smallAnswer;
+}
+int climbStairsRevisionMemo(int n)
+{
+    vector<int> dp(n + 1, -1);
+    dp[0] = dp[1] = 1;
+    helperClimbStairs(n, dp);
+    return dp[n];
+}
+// ----------------------
+int climbStairsRevisionDP(int n)
+{
+    vector<int> dp(n + 1, -1);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
 int main()
 {
     cout << string(35, '-') << endl;
-    string s = "aabb";
-    cout << firstUniqChar(s) << endl;
-    cout << string(35, '-');
+    cout << climbStairsRevisionDP(3);
+    cout << endl
+         << string(35, '-');
     return 0;
 }
