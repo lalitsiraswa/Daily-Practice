d913aa3d300dff139ecc5744599e98f70934e939
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-07-11 22:05:32 +0530

    70. Climbing Stairs

--- DP_PRACTICE.cpp
+++ DP_PRACTICE.cpp
@@ -43,10 +43,66 @@ int fibSpaceOptimization(int n)
     }
     return result;
 }
+// int main()
+// {
+//     cout << string(20, '-') << endl;
+//     cout << fibSpaceOptimization(4);
+//     cout << endl
+//          << string(20, '-');
+//     return 0;
+// }
+// --------------------------------------------------------------------- 70. Climbing Stairs -------------------------------------------------------------------------
+int climbStairsMemoization(int n, vector<int> &dp)
+{
+    if (n == 0)
+        return 1;
+    if (dp[n] != -1)
+        return dp[n];
+    int takeOneStep = climbStairsMemoization(n - 1, dp);
+    int takeTwoStep = 0;
+    if (n >= 2)
+        takeTwoStep = climbStairsMemoization(n - 2, dp);
+    return dp[n] = takeOneStep + takeTwoStep;
+}
+int climbStairs(int n)
+{
+    vector<int> dp(n + 1, -1);
+    climbStairsMemoization(n, dp);
+    return dp[n];
+}
+// -----------------------
+int climbStairsTabulation(int n)
+{
+    vector<int> dp(n + 1, -1);
+    dp[0] = 1;
+    for (int step = 1; step <= n; step++)
+    {
+        int takeOneStep = dp[step - 1];
+        int takeTwoStep = 0;
+        if (step >= 2)
+            takeTwoStep = dp[step - 2];
+        dp[step] = takeOneStep + takeTwoStep;
+    }
+    return dp[n];
+}
+// --------------------------
+int climbStairsSpaceOptimization(int n)
+{
+    if (n <= 1)
+        return n;
+    int lastSecond = 1, lastFirst = 1, result = 0;
+    for (int step = 2; step <= n; step++)
+    {
+        result = lastFirst + lastSecond;
+        lastSecond = lastFirst;
+        lastFirst = result;
+    }
+    return result;
+}
 int main()
 {
     cout << string(20, '-') << endl;
-    cout << fibSpaceOptimization(4);
+    cout << climbStairsSpaceOptimization(3);
     cout << endl
          << string(20, '-');
     return 0;
