cea76d0e2184f17cb7d6089efad83e750f924f53
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-07-13 00:29:10 +0530

    DP 3. Frog Jump | Dynamic Programming | Learn to write 1D DP

--- DP_PRACTICE.cpp
+++ DP_PRACTICE.cpp
@@ -168,11 +168,88 @@ bool canCrossPractice(vector<int> &stones)
         isPresent[stones[index]] = index;
     return canCrossHelper(1, 1, stones, dp, isPresent);
 }
+// int main()
+// {
+//     cout << string(20, '-') << endl;
+//     vector<int> stones = {0, 1, 3, 5, 6, 8, 12, 17};
+//     cout << canCrossPractice(stones);
+//     cout << endl
+//          << string(20, '-');
+//     return 0;
+// }
+// ---------------------------------- DP 3. Frog Jump | Dynamic Programming | Learn to write 1D DP ----------------------------------------
+int frogJumpTUF(vector<int> &heights, int index)
+{
+    if (index == heights.size() - 1)
+        return 0;
+    int takeOneStep = abs(heights[index + 1] - heights[index]) + frogJumpTUF(heights, index + 1);
+    int takeTwoStep = INT_MAX;
+    if (index < heights.size() - 2)
+        takeTwoStep = abs(heights[index + 2] - heights[index]) + frogJumpTUF(heights, index + 2);
+    return min(takeOneStep, takeTwoStep);
+}
+// ---------------
+
+int frogJumpTUFMemoizationRecusion(vector<int> &heights, int index, vector<int> &dp)
+{
+    if (index == 0)
+        return dp[index] = 0;
+    if (dp[index] != -1)
+        return dp[index];
+    int takeOneStep = abs(heights[index - 1] - heights[index]) + frogJumpTUFMemoizationRecusion(heights, index - 1, dp);
+    int takeTwoStep = INT_MAX;
+    if (index > 1)
+        takeTwoStep = abs(heights[index - 2] - heights[index]) + frogJumpTUFMemoizationRecusion(heights, index - 2, dp);
+    return dp[index] = min(takeOneStep, takeTwoStep);
+}
+int frogJumpTUFMemoization(vector<int> &heights, int index)
+{
+    int n = heights.size();
+    vector<int> dp(n, -1);
+    frogJumpTUFMemoizationRecusion(heights, n - 1, dp);
+    return dp[n - 1];
+}
+// -----------------
+int frogJumpTUFTabulation(vector<int> &heights)
+{
+    int n = heights.size();
+    vector<int> dp(n, -1);
+    dp[0] = 0;
+    for (int index = 1; index < n; index++)
+    {
+        int takeOneStep = abs(heights[index - 1] - heights[index]) + dp[index - 1];
+        int takeTwoStep = INT_MAX;
+        if (index > 1)
+            takeTwoStep = abs(heights[index - 2] - heights[index]) + dp[index - 2];
+        dp[index] = min(takeOneStep, takeTwoStep);
+    }
+    return dp[n - 1];
+}
+// ------------------------------
+int frogJumpTUFSpaceOptimization(vector<int> &heights)
+{
+    int n = heights.size();
+    int minEnergy = INT_MAX;
+    int firstStep = 0;
+    int secondStep = heights[1] - heights[0];
+    for (int index = 2; index < n; index++)
+    {
+
+        int takeOneStep = abs(heights[index - 1] - heights[index]) + secondStep;
+        int takeTwoStep = INT_MAX;
+        takeTwoStep = abs(heights[index - 2] - heights[index]) + firstStep;
+        minEnergy = min(takeOneStep, takeTwoStep);
+        firstStep = secondStep;
+        secondStep = minEnergy;
+    }
+    return minEnergy;
+}
 int main()
 {
     cout << string(20, '-') << endl;
-    vector<int> stones = {0, 1, 3, 5, 6, 8, 12, 17};
-    cout << canCrossPractice(stones);
+    vector<int> heights = {30, 10, 60, 10, 60, 50};
+    // cout << frogJumpTUFSpaceOptimization(heights, heights.size() - 1);
+    cout << frogJumpTUFTabulation(heights);
     cout << endl
          << string(20, '-');
     return 0;
