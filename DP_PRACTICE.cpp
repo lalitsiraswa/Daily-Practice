062f54a693d89c73108d3e3cd6d37c8ac0686bf6
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-07-14 17:25:36 +0530

    TUF : DP 7. Ninja's Training

--- DP_PRACTICE.cpp
+++ DP_PRACTICE.cpp
@@ -518,11 +518,50 @@ int ninjaTrainingTabulation(int n, vector<vector<int>> &points)
     // The maximum points for the last day with any activity can be found in dp[n-1][3]
     return dp[n - 1][3];
 }
+// -----------------
+// Function to find the maximum points for ninja training
+int ninjaTrainingSpaceOptimization(int n, vector<vector<int>> &points)
+{
+    // Initialize a vector to store the maximum points for the previous day's activities
+    vector<int> prev(4, 0);
+
+    // Initialize the DP table for the first day (day 0)
+    prev[0] = max(points[0][1], points[0][2]);
+    prev[1] = max(points[0][0], points[0][2]);
+    prev[2] = max(points[0][0], points[0][1]);
+    prev[3] = max(points[0][0], max(points[0][1], points[0][2]));
+
+    // Iterate through the days starting from day 1
+    for (int day = 1; day < n; day++)
+    {
+        // Create a temporary vector to store the maximum points for the current day's activities
+        vector<int> temp(4, 0);
+        for (int last = 0; last < 4; last++)
+        {
+            temp[last] = 0;
+            // Iterate through the tasks for the current day
+            for (int task = 0; task <= 2; task++)
+            {
+                if (task != last)
+                {
+                    // Calculate the points for the current activity and add it to the
+                    // maximum points obtained on the previous day (stored in prev)
+                    temp[last] = max(temp[last], points[day][task] + prev[task]);
+                }
+            }
+        }
+        // Update prev with the maximum points for the current day
+        prev = temp;
+    }
+
+    // The maximum points for the last day with any activity can be found in prev[3]
+    return prev[3];
+}
 int main()
 {
     cout << string(20, '-') << endl;
     vector<vector<int>> points = {{2, 1, 3}, {3, 4, 6}, {10, 1, 6}, {8, 3, 7}};
-    cout << ninjaTrainingTabulation(points.size(), points);
+    cout << ninjaTrainingSpaceOptimization(points.size(), points);
     cout << endl
          << string(20, '-');
     return 0;
