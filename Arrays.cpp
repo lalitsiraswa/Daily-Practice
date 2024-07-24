286d504942c5f0494d520f86ec3282c15b4fa642
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-07-24 19:47:36 +0530

    Largest Element in Array

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// -------------------------------------------------------------- Largest Element in Array ------------------------------------------------------------
+int largestElement(vector<int> &arr, int n)
+{
+    int largestElement = 0;
+    int leftPointer = 0, rightPointer = n - 1;
+    while (leftPointer <= rightPointer)
+    {
+        largestElement = max(largestElement, max(arr[leftPointer], arr[rightPointer]));
+        leftPointer++;
+        rightPointer--;
+    }
+    return largestElement;
+}
 int main()
 {
     cout << string(30, '-') << endl;
+    vector<int> arr = {3, 2, 1, 5, 2};
+    cout << largestElement(arr, arr.size());
     cout << endl
          << string(30, '-');
 }
