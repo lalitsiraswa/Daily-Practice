7191bd5e318d9f0380513b837d773b562cb37d8b
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-08-20 23:13:06 +0530

    1903. Largest Odd Number in String

--- Strings.cpp
+++ Strings.cpp
@@ -46,11 +46,99 @@ string removeOuterParentheses2(string s)
     }
     return result;
 }
+// int main()
+// {
+//     cout << string(35, '-') << endl;
+//     string s = "(()())(())(()(()))";
+//     cout << removeOuterParentheses2(s) << endl;
+//     cout << endl
+//          << string(35, '-');
+//     return 0;
+// }
+// ----------------------------------------------------------------- 151. Reverse Words in a String ---------------------------------------------------------------------
+string reverseWords(string s)
+{
+    int n = s.size();
+    string result;
+    string subString;
+    int low = 0, high = n - 1;
+    // Trim spaces from left and right end
+    while (s[low] == ' ' || s[high] == ' ')
+    {
+        if (s[low] == ' ')
+            low += 1;
+        if (s[high] == ' ')
+            high -= 1;
+    }
+    for (int index = low; index <= high; index++)
+    {
+        if (s[index] != ' ')
+            subString.push_back(s[index]);
+        else if (!subString.empty())
+        {
+            result = " " + subString + result;
+            subString = "";
+        }
+    }
+    result = subString + result;
+    return result;
+}
+// ------------------------
+string reverseWords2(string s)
+{
+    int n = s.size();
+    string result;
+    string subString;
+    for (int index = 0; index < n; index++)
+    {
+        if (s[index] != ' ')
+            subString.push_back(s[index]);
+        else if (!subString.empty())
+        {
+            if (result.empty())
+                result = subString;
+            else
+                result = subString + " " + result;
+            subString = "";
+        }
+    }
+    if (result.empty())
+        return subString;
+    if (!subString.empty())
+        result = subString + " " + result;
+    return result;
+}
+// int main()
+// {
+//     cout << string(35, '-') << endl;
+//     // string s = "  hello world  ";
+//     string s = "EPY2giL";
+//     cout << reverseWords2(s) << endl;
+//     cout << endl
+//          << string(35, '-');
+//     return 0;
+// }
+// ----------------------------------------------------------------- 1903. Largest Odd Number in String ---------------------------------------------------------------------
+string largestOddNumber(string num)
+{
+    int n = num.size();
+    int high = n - 1;
+    while (high >= 0)
+    {
+        if ((num[high] - '0') % 2 != 0)
+            break;
+        high--;
+    }
+    int low = 0;
+    return num.substr(0, high + 1);
+}
 int main()
 {
     cout << string(35, '-') << endl;
-    string s = "(()())(())(()(()))";
-    cout << removeOuterParentheses2(s) << endl;
+    // string num = "52";
+    // string num = "4206";
+    string num = "35427";
+    cout << largestOddNumber(num) << endl;
     cout << endl
          << string(35, '-');
     return 0;
