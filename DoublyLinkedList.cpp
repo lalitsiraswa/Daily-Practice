a3bd250f6768212923f034eda77fdf89dad8dae9
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-09-02 22:54:19 +0530

    delete head or tail of the linked list

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -23,6 +23,42 @@ Node<int> *array2DoublyLinkedList(vector<int> &vect)
     }
     return head;
 }
+Node<int> *deleteHead(Node<int> *head)
+{
+    if (head == nullptr)
+        return nullptr;
+    if (head->next == nullptr)
+    {
+        delete head;
+        return nullptr;
+    }
+    Node<int> *previous = head;
+    head = head->next;
+    head->previous = nullptr;
+    previous->next = nullptr;
+    delete previous;
+    return head;
+}
+Node<int> *deleteTail(Node<int> *head)
+{
+    if (head == nullptr)
+        return nullptr;
+    if (head->next == nullptr)
+    {
+        delete head;
+        return nullptr;
+    }
+    Node<int> *temp = head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    Node<int> *previous = temp->previous;
+    previous->next = nullptr;
+    temp->previous = nullptr;
+    delete temp;
+    return head;
+}
 void printDoublyLinkedList(Node<int> *head)
 {
     Node<int> *temp = head;
@@ -38,6 +74,10 @@ int main()
     cout << string(30, '-') << endl;
     vector<int> vect = {2, 4, 6, 8, 10, 12};
     Node<int> *head = array2DoublyLinkedList(vect);
+    printDoublyLinkedList(head);
+    head = deleteHead(head);
+    printDoublyLinkedList(head);
+    head = deleteTail(head);
     printDoublyLinkedList(head);
     cout << endl
          << string(30, '-') << endl;
