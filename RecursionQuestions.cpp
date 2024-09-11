3e723ba2bbba758050e444a9dd9fab71f79fbba5
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-09-11 23:07:13 +0530

    Print 1 to N By Backtracking or Without using + sign

--- RecursionQuestions.cpp
+++ RecursionQuestions.cpp
@@ -9,10 +9,26 @@ void printHelloWorld(int count)
     cout << "Hello World" << endl;
     printHelloWorld(count - 1);
 }
+// int main()
+// {
+//     cout << string(30, '-') << endl;
+//     printHelloWorld(5);
+//     cout << endl
+//          << string(30, '-') << endl;
+//     return 0;
+// }
+// ---------------------------------------------------- Print 1 to N By Backtracking or Without using + sign  -----------------------------------------------------------
+void print1ToNUsingBacktracking(int n)
+{
+    if (n == 0)
+        return;
+    print1ToNUsingBacktracking(n - 1);
+    cout << n << endl;
+}
 int main()
 {
     cout << string(30, '-') << endl;
-    printHelloWorld(5);
+    print1ToNUsingBacktracking(5);
     cout << endl
          << string(30, '-') << endl;
     return 0;
