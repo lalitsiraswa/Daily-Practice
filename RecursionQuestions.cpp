29babdb607811c6eeeb409e66b8e38a87514a1cf
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-09-11 22:54:01 +0530

    Pring Hello World N Times

--- RecursionQuestions.cpp
+++ RecursionQuestions.cpp
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// ----------------------------------------------------------------- Pring Hello World N Times --------------------------------------------------------------------------
+void printHelloWorld(int count)
+{
+    if (count == 0)
+        return;
+    cout << "Hello World" << endl;
+    printHelloWorld(count - 1);
+}
 int main()
 {
     cout << string(30, '-') << endl;
+    printHelloWorld(5);
     cout << endl
          << string(30, '-') << endl;
     return 0;
