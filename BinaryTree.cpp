253765e11cf07076118b2bb015c5aab03e5015a6
lalitsiraswa <lalitsiraswa1999@gmail.com>
2024-11-01 14:24:43 +0530

    987. Vertical Order Traversal of a Binary Tree

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -816,22 +816,143 @@ vector<int> boundaryTUF(TreeNode *root)
     addRightBoundary(root, result);
     return result;
 }
+// int main()
+// {
+//     cout << string(35, '-') << endl;
+//     TreeNode *root = new TreeNode(1);
+//     root->left = new TreeNode(100);
+//     root->right = new TreeNode(2);
+//     root->right->right = new TreeNode(3);
+//     root->right->right->right = new TreeNode(4);
+//     root->right->right->right->right = new TreeNode(5);
+//     root->right->right->right->right->right = new TreeNode(6);
+//     vector<int> result = boundary(root);
+//     for (int i = 0; i < result.size(); i++)
+//     {
+//         cout << result[i] << ", ";
+//     }
+//     cout << endl
+//          << string(35, '-') << endl;
+//     return 0;
+// }
+// --------------------------------------------------------------- 987. Vertical Order Traversal of a Binary Tree ---------------------------------------------------------------------
+void verticalTraversal(TreeNode *root, map<int, map<int, vector<int>>> &mp, int row, int column)
+{
+    if (root == nullptr)
+        return;
+    // Check if the column is exists or not
+    if (mp.count(column))
+    {
+        // Check if the the row exits ot not
+        if (mp[column].count(row))
+        {
+            mp[column][row].push_back(root->val);
+            sort(mp[column][row].begin(), mp[column][row].end());
+        }
+        // if not add a new row;
+        else
+        {
+            mp[column][row] = {root->val};
+        }
+    }
+    // If column doesn't exists
+    else
+    {
+        mp[column][row] = {root->val};
+    }
+    verticalTraversal(root->left, mp, row + 1, column - 1);
+    verticalTraversal(root->right, mp, row + 1, column + 1);
+}
+vector<vector<int>> verticalTraversal(TreeNode *root)
+{
+    vector<vector<int>> result;
+    // map<column, map<row, vector<int>>> mp;
+    map<int, map<int, vector<int>>> mp;
+    verticalTraversal(root, mp, 0, 0);
+    for (auto &col : mp)
+    {
+        vector<int> temp;
+        for (auto &row : col.second)
+        {
+            temp.insert(temp.end(), row.second.begin(), row.second.end());
+        }
+        result.push_back(temp);
+    }
+    return result;
+}
+// ----------------------------------------------------
+vector<vector<int>> verticalTraversalTUF(TreeNode *root)
+{
+    // Map to store nodes based on vertical and level information
+    map<int, map<int, multiset<int>>> nodes;
+    // Queue for BFS traversal, each element is a pair containing node and its vertical and level information
+    // vertical and level are like row and column
+    queue<pair<TreeNode *, pair<int, int>>> todo;
+    // Push the root node with initial vertical and level values (0, 0)
+    todo.push({root, {0, 0}});
+    // BFS traversal
+    while (!todo.empty())
+    {
+        // Retrieve the node and its vertical and level information from the front of the queue
+        auto p = todo.front();
+        todo.pop();
+        TreeNode *temp = p.first;
+        // Extract the vertical and level information (row & column information)
+        // x -> vertical
+        int x = p.second.first;
+        // y -> level
+        int y = p.second.second;
+        // Insert the node value into the corresponding vertical and level in the map
+        nodes[x][y].insert(temp->val);
+        // Process left child
+        if (temp->left)
+        {
+            // Move left in terms of vertical and
+            // Move down in terms of level
+            todo.push({temp->left, {x - 1, y + 1}});
+        }
+        // Process right child
+        if (temp->right)
+        {
+            // Move right interms of vertical and
+            // Move down in terms of level
+            todo.push({temp->right, {x + 1, y + 1}});
+        }
+    }
+    // Prepare the final result vector by combining values from the map
+    vector<vector<int>> result;
+    for (auto p : nodes)
+    {
+        vector<int> col;
+        for (auto q : p.second)
+        {
+            // Insert node values into the column vector
+            col.insert(col.end(), q.second.begin(), q.second.end());
+        }
+        // Add the column vector to the final result
+        result.push_back(col);
+    }
+    return result;
+}
 int main()
 {
     cout << string(35, '-') << endl;
     TreeNode *root = new TreeNode(1);
-    root->left = new TreeNode(100);
-    root->right = new TreeNode(2);
-    root->right->right = new TreeNode(3);
-    root->right->right->right = new TreeNode(4);
-    root->right->right->right->right = new TreeNode(5);
-    root->right->right->right->right->right = new TreeNode(6);
-    vector<int> result = boundary(root);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+    vector<vector<int>> result = verticalTraversalTUF(root);
     for (int i = 0; i < result.size(); i++)
     {
-        cout << result[i] << ", ";
+        cout << "{";
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            cout << result[i][j] << ", ";
+        }
+        cout << "}";
     }
-    cout << endl
-         << string(35, '-') << endl;
     return 0;
 }
