d5735146633d8832e0baec25cd856f4c947d8ecc
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2024-11-28 22:17:58 +0530

    Partitions with Given Difference

--- DP_PRACTICE.cpp
+++ DP_PRACTICE.cpp
@@ -3843,11 +3843,51 @@ int perfectSumSpaceOptimization(vector<int> &arr, int target)
     }
     return prev[target];
 }
+// int main()
+// {
+//     cout << string(20, '-') << endl;
+//     vector<int> arr = {27, 0, 24};
+//     cout << perfectSumSpaceOptimization(arr, 24);
+//     cout << endl
+//          << string(20, '-');
+//     return 0;
+// }
+// ------------------------------------------------------------------- Partitions with Given Difference --------------------------------------------------------------------------
+int countPartitions(vector<int> &arr, int index, int target, vector<vector<int>> &dp)
+{
+    if (index == 0)
+    {
+        if (target == 0 && arr[0] == 0)
+            return dp[index][target] = 2;
+        if (target == 0 || target == arr[0])
+            return dp[index][target] = 1;
+        return dp[index][target] = 0;
+    }
+    if (dp[index][target] != -1)
+    {
+        return dp[index][target];
+    }
+    int notTake = countPartitions(arr, index - 1, target, dp);
+    int take = 0;
+    if (target >= arr[index])
+    {
+        take = countPartitions(arr, index - 1, target - arr[index], dp);
+    }
+    return dp[index][target] = (take + notTake);
+}
+int countPartitionsRevision(vector<int> &arr, int d)
+{
+    int n = arr.size();
+    int totalSum = accumulate(arr.begin(), arr.end(), 0);
+    int target = (totalSum - d) / 2;
+    vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+    return countPartitions(arr, n - 1, target, dp);
+}
 int main()
 {
     cout << string(20, '-') << endl;
-    vector<int> arr = {27, 0, 24};
-    cout << perfectSumSpaceOptimization(arr, 24);
+    vector<int> arr = {1, 3, 3, 2, 1};
+    cout << countPartitionsRevision(arr, 5);
     cout << endl
          << string(20, '-');
     return 0;
