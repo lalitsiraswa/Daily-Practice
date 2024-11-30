b2a7ceafe4cd41374e9046e60cb0792615360714
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2024-11-30 17:42:06 +0530

    104. Maximum Depth of Binary Tree

---
+++ TreePractice.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//   Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// -------------------------------------------------------------------------- 104. Maximum Depth of Binary Tree ---------------------------------------------------------------------
+int maxDepth(TreeNode *root)
+{
+    if (root == nullptr)
+    {
+        return 0;
+    }
+    queue<TreeNode *> que;
+    que.push(root);
+    int treeDepth = 0;
+    while (!que.empty())
+    {
+        treeDepth += 1;
+        int size = que.size();
+        for (int i = 1; i <= size; i++)
+        {
+            TreeNode *frontNode = que.front();
+            que.pop();
+            if (frontNode->left)
+                que.push(frontNode->left);
+            if (frontNode->right)
+                que.push(frontNode->right);
+        }
+    }
+    return treeDepth;
+}
+// ---------------------
+void maxDepth(TreeNode *root, int level, int &maxHeight)
+{
+    if (root == nullptr)
+        return;
+    if (level > maxHeight)
+        maxHeight = level;
+    maxDepth(root->left, level + 1, maxHeight);
+    maxDepth(root->right, level + 1, maxHeight);
+}
+int maxDepthRecursion(TreeNode *root)
+{
+    if (root == nullptr)
+    {
+        return 0;
+    }
+    int maxHeight = 0;
+    maxDepth(root, 1, maxHeight);
+    return maxHeight;
+}
+// ------------------------
+int maxDepthOtherWay(TreeNode *root)
+{
+    if (root == nullptr)
+        return 0;
+    int leftSubTreeDepth = maxDepth(root->left);
+    int rightSubTreeDepth = maxDepth(root->right);
+    return 1 + max(leftSubTreeDepth, rightSubTreeDepth);
+}
+int main()
+{
+    cout << string(35, '-') << endl;
+    TreeNode *root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+    cout << maxDepthOtherWay(root) << endl;
+    cout << endl
+         << string(35, '-');
+    return 0;
+}
