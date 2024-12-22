e138af686acf0604edc2ed9b7a054dd55b399e14
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2024-12-22 22:52:18 +0530

    153. Find Minimum in Rotated Sorted Array Revision

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1449,11 +1449,55 @@ bool search2Revision(vector<int> &nums, int target)
     }
     return false;
 }
+// int main()
+// {
+//     cout << string(30, '-') << endl;
+//     vector<int> nums = {3, 1, 2, 3, 3, 3, 3};
+//     cout << search2Revision(nums, 1) << endl;
+//     cout << endl
+//          << string(30, '-');
+// }
+// --------------------------------------------------------------------- 153. Find Minimum in Rotated Sorted Array Revision ------------------------------------------------------------------------------
+int findMinRevision(vector<int> &nums)
+{
+    int left = 0;
+    int right = nums.size() - 1;
+    int minValue = INT_MAX;
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+        // search space is already sorted
+        // then arr[low] will always be
+        // the minimum in that search space:
+        if (nums[left] <= nums[right])
+        {
+            minValue = min(minValue, nums[left]);
+            break;
+        }
+        // if left part is sorted:
+        if (nums[left] <= nums[mid])
+        {
+            // keep the minimum:
+            minValue = min(minValue, nums[left]);
+            // Eliminate left half:
+            left = mid + 1;
+        }
+        // if right part is sorted:
+        else
+        {
+            // keep the minimum:
+            minValue = min(minValue, nums[mid]);
+            // Eliminate right half:
+            right = mid - 1;
+        }
+    }
+    return minValue;
+}
 int main()
 {
     cout << string(30, '-') << endl;
-    vector<int> nums = {3, 1, 2, 3, 3, 3, 3};
-    cout << search2Revision(nums, 1) << endl;
+    vector<int> nums = {3, 4, 5, 1, 2};
+    cout << findMinRevision(nums) << endl;
     cout << endl
          << string(30, '-');
 }
