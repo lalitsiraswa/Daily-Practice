8a36c4018a14e1d795d518ea5a639dfa2fc72099
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-01-01 13:58:11 +0530

    662. Maximum Width of Binary Tree

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1684,12 +1684,20 @@ int main()
 {
     cout << string(35, '-') << endl;
     TreeNode *root = new TreeNode(1);
-    root->left = new TreeNode(3);
-    root->right = new TreeNode(2);
-    root->left->left = new TreeNode(5);
-    root->left->left->left = new TreeNode(6);
-    root->right->right = new TreeNode(9);
-    root->right->right->left = new TreeNode(7);
+    root->left = new TreeNode(2);
+    root->left->left = new TreeNode(4);
+    root->left->left->left = new TreeNode(8);
+    root->left->left->right = new TreeNode(9);
+    root->left->right = new TreeNode(5);
+    root->left->right->left = new TreeNode(10);
+    root->left->right->right = new TreeNode(11);
+    root->right = new TreeNode(3);
+    root->right->left = new TreeNode(6);
+    root->right->left->left = new TreeNode(12);
+    root->right->left->right = new TreeNode(13);
+    root->right->right = new TreeNode(7);
+    root->right->right->left = new TreeNode(14);
+    root->right->right->right = new TreeNode(15);
     cout << widthOfBinaryTreeTuf(root);
     cout << endl
          << string(35, '-') << endl;
