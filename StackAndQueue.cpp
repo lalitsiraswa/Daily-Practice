ef3d0872c75f5723280519224677e0cb8be27e87
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-01-22 07:24:25 +0530

    225. Implement Stack using Queues

--- StackAndQueue.cpp
+++ StackAndQueue.cpp
@@ -44,14 +44,43 @@ public:
         return mainQueue.empty();
     }
 };
+// int main()
+// {
+//     cout << string(30, '-') << endl;
+//     MyStack stk;
+//     stk.push(1);
+//     stk.push(2);
+//     cout << stk.top() << endl;
+//     cout << stk.pop() << endl;
+//     cout << string(30, '-') << endl;
+//     return 1;
+// }
+// ------------------------------------------------------------------------------ 225. Implement Stack using Queues ---------------------------------------------------------------------------
+bool isValid(string s)
+{
+    stack<char> bracketStack;
+    for (char bracket : s)
+    {
+        if (bracket == '(' || bracket == '{' || bracket == '[')
+            bracketStack.push(bracket);
+        else if (bracketStack.empty())
+            return false;
+        else if (bracket == ')' && bracketStack.top() == '(')
+            bracketStack.pop();
+        else if (bracket == '}' && bracketStack.top() == '{')
+            bracketStack.pop();
+        else if (bracket == ']' && bracketStack.top() == '[')
+            bracketStack.pop();
+        else
+            return false;
+    }
+    return bracketStack.empty();
+}
 int main()
 {
     cout << string(30, '-') << endl;
-    MyStack stk;
-    stk.push(1);
-    stk.push(2);
-    cout << stk.top() << endl;
-    cout << stk.pop() << endl;
+    string s = "()[]{{}";
+    cout << isValid(s) << endl;
     cout << string(30, '-') << endl;
     return 1;
 }
