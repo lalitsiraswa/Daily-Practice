688b63a4eb85ce19451ddaf1720c39bf6d004ac9
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-01-28 22:42:07 +0530

    Infix to Postfix

--- StackAndQueue.cpp
+++ StackAndQueue.cpp
@@ -260,9 +260,71 @@ public:
         return s.back().second;
     }
 };
+// int main()
+// {
+//     cout << string(30, '-') << endl;
+//     cout << string(30, '-') << endl;
+//     return 1;
+// }
+// --------------------------------------------------------------------------------- Infix to Postfix ------------------------------------------------------------------------------
+// Function to return precedence of operators:
+int precedence(char ch)
+{
+    if (ch == '^')
+        return 3;
+    if (ch == '*' || ch == '/')
+        return 2;
+    if (ch == '+' || ch == '-')
+        return 1;
+    return -1;
+}
+string infixToPostfix(string &s)
+{
+    stack<char> stk;
+    string result;
+    for (int i = 0; i < s.size(); i++)
+    {
+        char ch = s[i];
+        // If the scanned character is an operand, add it to output string:
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
+            result.push_back(ch);
+        // If the scanned character is an ‘(‘, push it to the stack:
+        else if (ch == '(')
+            stk.push(ch);
+        // If the scanned character is an ‘)’, pop and to output string from the stack until an ‘(‘ is encountered:
+        else if (ch == ')')
+        {
+            while (!stk.empty() && stk.top() != '(')
+            {
+                result.push_back(stk.top());
+                stk.pop();
+            }
+            stk.pop();
+        }
+        // If an operator is scanned:
+        else
+        {
+            while (!stk.empty() && precedence(ch) <= precedence(stk.top()))
+            {
+                result.push_back(stk.top());
+                stk.pop();
+            }
+            stk.push(ch);
+        }
+    }
+    // Pop all the remaining elements from the stack:
+    while (!stk.empty())
+    {
+        result.push_back(stk.top());
+        stk.pop();
+    }
+    return result;
+}
 int main()
 {
     cout << string(30, '-') << endl;
+    string s = "a+b*(c^d-e)^(f+g*h)-i";
+    cout << infixToPostfix(s) << endl;
     cout << string(30, '-') << endl;
     return 1;
 }
