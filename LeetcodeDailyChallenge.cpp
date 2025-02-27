492271532812cdc753494771fd537e1436b9900d
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-02-27 07:34:18 +0530

    560. Subarray Sum Equals K

--- LeetcodeDailyChallenge.cpp
+++ LeetcodeDailyChallenge.cpp
@@ -28,11 +28,38 @@ int longestSubarray(vector<int> &arr, int k)
     }
     return maxLen;
 }
+// int main()
+// {
+//     cout << string(30, '-') << endl;
+//     vector<int> arr = {10, 5, 2, 7, 1, -10};
+//     cout << longestSubarray(arr, 15);
+//     cout << endl
+//          << string(30, '-') << endl;
+//     return 1;
+// }
+// ------------------------------------------------------------------------------ 560. Subarray Sum Equals K ---------------------------------------------------------------------------
+int subarraySum(vector<int> &nums, int k)
+{
+    map<long long, int> preSumMap;
+    int sum = 0;
+    int subarrayCount = 0;
+    for (int index = 0; index < nums.size(); index++)
+    {
+        sum += nums[index];
+        if (sum == k)
+            subarrayCount++;
+        int remaining = sum - k;
+        if (preSumMap.find(remaining) != preSumMap.end())
+            subarrayCount += preSumMap[remaining];
+        preSumMap[sum] = preSumMap[sum] + 1;
+    }
+    return subarrayCount;
+}
 int main()
 {
     cout << string(30, '-') << endl;
-    vector<int> arr = {10, 5, 2, 7, 1, -10};
-    cout << longestSubarray(arr, 15);
+    vector<int> nums = {1, 2, 3};
+    cout << subarraySum(nums, 3);
     cout << endl
          << string(30, '-') << endl;
     return 1;
