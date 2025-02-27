3db4e81dcfb41c559552392d9ac73174f3776dd9
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-02-27 22:46:52 +0530

    1524. Number of Sub-arrays With Odd Sum

--- LeetcodeDailyChallenge.cpp
+++ LeetcodeDailyChallenge.cpp
@@ -55,6 +55,42 @@ int subarraySum(vector<int> &nums, int k)
     }
     return subarrayCount;
 }
+// int main()
+// {
+//     cout << string(30, '-') << endl;
+//     vector<int> nums = {1, 2, 3};
+//     cout << subarraySum(nums, 3);
+//     cout << endl
+//          << string(30, '-') << endl;
+//     return 1;
+// }
+// ------------------------------------------------------------------------------ 1524. Number of Sub-arrays With Odd Sum ---------------------------------------------------------------------------
+int numOfSubarrays(vector<int> &arr)
+{
+    int MOD = (int)(1e9 + 7);
+    int count = 0, prefixSum = 0;
+    // evenCount starts as 1 since the initial sum (0) is even:
+    int oddCount = 0, evenCount = 1;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        prefixSum += arr[i];
+        // If current prefix sum is even, add the number of odd subarrays:
+        if (prefixSum % 2 == 0)
+        {
+            count += oddCount;
+            evenCount += 1;
+        }
+        // If current prefix sum is odd, add the number of even subarrays:
+        else
+        {
+            count += evenCount;
+            oddCount += 1;
+        }
+        // To handle large results:
+        count %= MOD;
+    }
+    return count;
+}
 int main()
 {
     cout << string(30, '-') << endl;
